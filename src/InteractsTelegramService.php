0cd7d25076afd0bbfeae2a8f3b98191652e1ba40
Savatneev Anton <savatneev-anton@mail.ru>
2022-06-20 21:10:55 +0300

    fix telegram morphOne

--- src/InteractsTelegramService.php
+++ src/InteractsTelegramService.php
@@ -3,15 +3,25 @@
 namespace Icekristal\LaravelTelegram;
 
 use Icekristal\LaravelTelegram\Models\ServiceTelegram;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait InteractsTelegramService
 {
     /**
      *
+     * @return MorphOne
+     */
+    public function telegram(): MorphOne
+    {
+        return $this->morphOne(ServiceTelegram::class, 'owner');
+    }
+
+    /**
+     *
      * @return MorphMany
      */
-    public function telegram(): MorphMany
+    public function telegrams(): MorphMany
     {
         return $this->morphMany(ServiceTelegram::class, 'owner');
     }
