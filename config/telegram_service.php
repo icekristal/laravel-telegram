fb185ea93b92f35d4dfcbcc5ea979263691b10ec
Savatneev Anton <savatneev-anton@mail.ru>
2022-06-20 21:42:01 +0300

    update config

--- config/telegram_service.php
+++ config/telegram_service.php
@@ -2,10 +2,12 @@
 return [
     'bots' => [
         'first_bot' => [
+            'name_bot' => 'first_bot',
             'is_save_database' => env('TELEGRAM_BOT_IS_SAVE_DATABASE', false),
             'is_technical_job' => env('TELEGRAM_BOT_IS_TECHNICAL_JOB', false),
             'is_save_files' => false,
             'path_save_files' => 'storage/telegram/',
+            'url' => 'https://t.me/' . env('TELEGRAM_BOT_NAME', 'first_bot'),
             'name' => env('TELEGRAM_BOT_NAME', null),
             'token' => env('TELEGRAM_BOT_TOKEN', null),
             'webhook_domain' => env('TELEGRAM_WEBHOOK_DOMAIN', null), //null - main domain
