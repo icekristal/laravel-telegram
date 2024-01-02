642785eb74a5aef9c1100d7208e39eb0ee954a2d
icekristal <savatneev-anton@mail.ru>
2024-01-02 15:07:42 +0300

    update info

--- src/Channels/TelegramChannel.php
+++ src/Channels/TelegramChannel.php
@@ -17,7 +17,7 @@ class TelegramChannel
         $sendInfo->setParams([
             'text' => $message->content
         ])->sendMessage();
-        $message->sendedMessageId($sendInfo->getSendedMessageId());
-        return $message;
+        $notifiable->sendedMessageId = $sendInfo->getSendedMessageId() ?? null;
+        return $sendInfo;
     }
 }
