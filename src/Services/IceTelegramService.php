7bf906a4f4dbcec06a6405087425b94714a7827a
icekristal <savatneev-anton@mail.ru>
2025-08-08 12:57:41 +0300

    update del after send photo

--- src/Services/IceTelegramService.php
+++ src/Services/IceTelegramService.php
@@ -165,6 +165,10 @@ class IceTelegramService
 
     public function sendPhoto(array $params, string $url): void
     {
+        $params['message_id'] = $this->messageId;
+        if (isset($params['is_delete_last_message']) && $params['is_delete_last_message']) {
+            $this->deleteMessage($params);
+        }
         $params['photo'] = $url;
         $params['chat_id'] = $params['chat_id'] ?? $this->from['id'];
         IceTelegram::setInfoBot($this->infoBot)->setParams($params)->sendPhoto();
